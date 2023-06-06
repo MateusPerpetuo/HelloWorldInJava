9d4f90f0f871d96f92bc862d40710ca51a33f7c7
Mateus <129229556+MateusPerpetuo@users.noreply.github.com>
2023-06-06 12:21:11 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+/**
+ * @author mateus.perpetuo
+ */
+public class HelloWorld {
+
+   
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+        System.out.println("I am Mateus and this is how I started");   
+    }
+}
